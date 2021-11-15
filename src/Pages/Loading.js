1b30fffaa60b51bef816d05a876bb4fbf012d48e
dilip20222 <dilips.stacksum@gmail.com>
2021-11-16 01:00:31 +0530

    Loading Image Change

--- src/Pages/Loading.js
+++ src/Pages/Loading.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 export const Loading = () => {
     return (
+        <>
         <div style={{width:'100%' , height:'100vh' , display:'flex' , flexDirection:'column' , justifyContent:'center' , alignItems:'center'}} >
-            <img src="..\Img\png-transparent-emoji-emoticon-gesture-the-finger-index-finger-index-finger-angle-hand-orange-thumbnail.png" alt="" width="800px" height='700px'/>
-            <h1>Welcome...! Site is Loading</h1>
+            <h1 style={{color:"crimson"}}>Welcome PiKa Pika</h1>
+            <img src="..\Img\pikachu.png" alt="" width="500px" height='500px'/>
         </div>
+        </>
     )
 }
