5551637ef660b77b8498ccf8b1d6acb5f35401cf
Dilip Suthar <suthardips123000@gmail.com>
2021-11-15 17:35:12 +0530

    Update : Logo updated

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const Nav = () => {
-    return (
-<>
-       <nav className="navbar">
-  <div className="max-width">
-    <div className="logo"> <a href>Portfo<span>lio.</span></a></div>
-    <ul className="menu">
-      <li> <Link to='/' className="menu-btn"></Link></li>
-    </ul>
-  </div>
-  </nav>
+  return (
+    <>
+      <nav className="navbar">
+        <div className="max-width">
+          <div className="logo">
+            {" "}
+            <a href>
+              Dilip<span>Suthar</span>
+            </a>
+          </div>
+          <ul className="menu">
+            <li>
+              {" "}
+              <Link to="/" className="menu-btn"></Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </>
-    )
-}
+  );
+};
 
 export default Nav;
