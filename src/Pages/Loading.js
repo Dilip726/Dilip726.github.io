fe7f5eee02ac4b6558a75bfe7ae7ccd16d584e46
dilip20222 <dilips.stacksum@gmail.com>
2021-11-16 00:49:43 +0530

    experience secction added and loader added with 2 sec delay

--- src/Pages/Loading.js
+++ src/Pages/Loading.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Loading = () => {
     return (
         <div style={{width:'100%' , height:'100vh' , display:'flex' , flexDirection:'column' , justifyContent:'center' , alignItems:'center'}} >
-            <img src="..\Img\190-1908713_pikachu-clipart-png-icon-transparent-background-pikachu-png.png" alt="" width="800px" height='700px'/>
+            <img src="..\Img\png-transparent-emoji-emoticon-gesture-the-finger-index-finger-index-finger-angle-hand-orange-thumbnail.png" alt="" width="800px" height='700px'/>
             <h1>Welcome...! Site is Loading</h1>
         </div>
     )
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,34 +1,36 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
 export const Nav = () => {
-  const [onScroll, setOnScroll] = React.useState(0);
-  const [visible, setVisible] = useState(true);
-  const navbarStyle = {
-    position: "fixed",
-    top: "5px",
-    left: "0px",
-  };
+  // const [onScroll, setOnScroll] = React.useState(0);
+  // const [visible, setVisible] = useState(true);
+  // const navbarStyle = {
+  //   position: "fixed",
+  //   top: "5px",
+  //   left: "0px",
+  // };
 
   
-  const handleOnScroll = () => {
-    const currentScroll = window.pageYOffset;
-    setVisible(
-      (onScroll > currentScroll && onScroll - currentScroll > 70) ||
-        currentScroll < 10
-    );
-    setOnScroll(currentScroll);
-  };
+  // const handleOnScroll = () => {
+  //   const currentScroll = window.pageYOffset;
+  //   setVisible(
+  //     (onScroll > currentScroll && onScroll - currentScroll > 70) ||
+  //       currentScroll < 10
+  //   );
+  //   setOnScroll(currentScroll);
+  // };
 
-  React.useEffect(() => {
-    window.addEventListener("scroll", handleOnScroll);
+  // React.useEffect(() => {
+  //   window.addEventListener("scroll", handleOnScroll);
 
-    return () => window.removeEventListener("scroll", handleOnScroll);
-  }, [onScroll, visible, handleOnScroll]);
+  //   return () => window.removeEventListener("scroll", handleOnScroll);
+  // }, [onScroll, visible]);
+
+  // style={{ ...navbarStyle  , top: visible ? '0' : '-60px'}}
 
   return (
     <>
-      <nav className="navbar" style={{ ...navbarStyle  , top: visible ? '0' : '-60px'}}>
+      <nav className="navbar" >
         <div className="max-width">
           <div className="logo" >
             {" "}
