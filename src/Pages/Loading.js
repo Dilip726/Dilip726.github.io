3e6b5d20ba60b80d298e695feebcb3c9da30051b
Dilip Suthar <84769228+SutharDilip@users.noreply.github.com>
2021-11-16 11:53:23 +0530

    Update Loading

--- src/Pages/Loading.js
+++ src/Pages/Loading.js
@@ -3,9 +3,14 @@ import React from 'react'
 export const Loading = () => {
     return (
         <>
-        <div style={{width:'100%' , height:'100vh' , display:'flex' , flexDirection:'column' , justifyContent:'center' , alignItems:'center'}} >
-            <img src="https://images.unsplash.com/photo-1595853035070-59a39fe84de3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=725&q=80" alt="" width="100vw" height='100vh'/>
-        </div>
+         <div style={{ width: "100vw", height: "100vh" }}>
+        <img
+          src="https://images.unsplash.com/photo-1543332164-6e82f355badc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80"
+          alt=""
+          width="100%"
+          height="100%"
+        />
+      </div>
         </>
     )
 }
