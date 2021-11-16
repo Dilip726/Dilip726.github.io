40e4da88da684ec72151bea55ed08eadfb2412d0
Dilip Suthar <84769228+SutharDilip@users.noreply.github.com>
2021-11-16 11:34:30 +0530

    Update Loading.js

--- src/Pages/Loading.js
+++ src/Pages/Loading.js
@@ -4,7 +4,7 @@ export const Loading = () => {
     return (
         <>
         <div style={{width:'100%' , height:'100vh' , display:'flex' , flexDirection:'column' , justifyContent:'center' , alignItems:'center'}} >
-            <h1 style={{color:"crimson"}}>Welcome PiKa Pika</h1>
+            <h1 style={{color:"crimson"}}>Welcome</h1>
             <img src="..\Img\pikachu.png" alt="" width="500px" height='500px'/>
         </div>
         </>
